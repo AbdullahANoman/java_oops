1c0d3fab27b387e3f8c28d8cd8950b14d71d09a9
Abdullah Al Noman <abdullahnoman4537@gmail.com>
2025-01-12 13:14:16 +0600

    learn thread in java

---
+++ ThreadJava.java
@@ -0,0 +1,44 @@
+class Pokemon implements Runnable{
+    String name;
+    String attack;
+   
+    public Pokemon(String name, String attack){
+        this.name = name;
+        this.attack = attack;
+    }
+   
+    @Override
+    public void run(){
+        for(int i=0; i<5; i++){
+            synchronized(Pokemon.class){
+                System.out.println(name+" used "+attack);
+                try{
+                    Thread.sleep(1000);
+                    Pokemon.class.notify();
+                    if(i<4){
+                        Pokemon.class.wait();
+                    }
+                }
+                catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+
+        Pokemon pika = new Pokemon("Pikachu", "Thunderbolt");
+        Pokemon zard = new Pokemon("Charizard", "Flamethrower");
+       
+       
+        Thread t1 = new Thread(pika);
+        Thread t2 = new Thread(zard);
+       
+        t1.start();
+        t2.start();
+
+    }
+}
